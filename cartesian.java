68e302566d40f54f8a7064243ff401be1e67103a
nithunkarur <rknithun@gmail.com>
2021-10-13 20:35:29 +0530

    Using equals method to check the length of two lines

--- cartesian.java
+++ cartesian.java
@@ -17,6 +17,25 @@ class cartesian{
 
         double line1=Math.sqrt(Math.pow(x1-x2,2)+Math.pow(y1-y2,2));
         System.out.println(line1);	
+	
+	System.out.println("enter the xx1 coordinates");
+        xx1=sc.nextDouble();
+        System.out.println("enter the xx2 coordinates");
+        xx2=sc.nextDouble();
+        System.out.println("enter the yy1 coordinates");
+        yy1=sc.nextDouble();
+        System.out.println("enter the yy2 coordinates");
+        yy2=sc.nextDouble();
+
+        double line2=Math.sqrt(Math.pow(xx1-xx2,2)+Math.pow(yy1-yy2,2));
+        System.out.println(line2);
+	Double l1=line1;
+	Double l2=line2;	
+	if (l1.equals(l2)){
+		System.out.println("two line are equal");
+	}
+	else
+		System.out.println("two lines are not equal");
 	}
 }
 

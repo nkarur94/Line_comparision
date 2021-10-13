bf456da95ff7f0f61b6a8c51220b810b87c37356
nithunkarur <rknithun@gmail.com>
2021-10-13 20:05:55 +0530

    welcome note

---
+++ cartesian.java
@@ -0,0 +1,8 @@
+class cartesian{
+	public static void main(String[] args){
+	System.out.println("welcome to line comparition computational progarm");
+	System.out.println("*************************************************");	
+	
+	}
+}
+
